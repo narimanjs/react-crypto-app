5ae7c0a6322595411796aa63e21172a29c7717a0
Nariman <mirmanov1@mail.ru>
2024-02-21 23:30:25 -0800

    first commit

---
+++ src/components/layout/AppSider/AppSider.jsx
@@ -0,0 +1,20 @@
+import { Layout } from 'antd';
+
+const siderStyle = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#1677ff',
+};
+const AppSider = () => {
+  return (
+    <Layout.Sider
+      width='25%'
+      style={siderStyle}
+    >
+      Sider
+    </Layout.Sider>
+  );
+};
+
+export default AppSider;
